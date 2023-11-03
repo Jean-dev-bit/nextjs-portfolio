4e0ed1d1bcaf89bbf34b5cef33897c09ddb6688f
Yémalin <119426428+Jean-dev-bit@users.noreply.github.com>
2023-11-03 11:04:38 +0100

    Update Projects.jsx

--- src/app/Components/Projects.jsx
+++ src/app/Components/Projects.jsx
@@ -75,7 +75,7 @@ const cardVariants = {
   animate: {y: 0, opacity: 1},
 };
   return (
-    <section>
+    <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">My projects</h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag  
