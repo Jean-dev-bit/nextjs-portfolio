bc422031856c3975477fc0beebe0ece9ad92b0b1
Yémalin <119426428+Jean-dev-bit@users.noreply.github.com>
2023-11-03 11:03:41 +0100

    Update AboutSection.jsx

--- src/app/Components/AboutSection.jsx
+++ src/app/Components/AboutSection.jsx
@@ -58,7 +58,7 @@ const AboutSection = () => {
         setTab(id);
     });
   }
-  return <section className="text-white">
+  return <section className="text-white" id="about">
         <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
             <Image
                 src="/images/im3.jpg"
